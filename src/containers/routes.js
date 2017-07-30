9fe2fc6b8702ed5cc07ff7ac5e2e35ade87304de
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-07-29 21:35:19 -0700

    actually fixed it this time

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -3,9 +3,6 @@ import {Route, Switch} from 'react-router-dom'
 import ProfessorContainer from './professorContainer'
 import StudentContainer from './studentContainer'
 import LoginContainer from './loginContainer'
-import StudentSignup from '../components/studentSignup'
-import ProfessorSignup from '../components/professorSignup'
-import EnterCode from '../components/enterCode'
 
 export default class Routes extends React.Component {
   constructor(props){
@@ -16,12 +13,9 @@ export default class Routes extends React.Component {
     return(
       <div>
         <Switch>
-              <Route path='/student/enterCode' component={EnterCode}/>
               <Route path="/" exact={true} component={LoginContainer}/>
-              <Route path="/student/signup" component={StudentSignup}/>
-              <Route path="/professor/signup" component={ProfessorSignup}/>
-              <Route path="/professor" exact={true} component={ProfessorContainer}/>
-              <Route path="/student" exact={true} component={StudentContainer}/>
+              <Route path="/professor" component={ProfessorContainer}/>
+              <Route path="/student" component={StudentContainer}/>
         </Switch>
     </div>
     )
