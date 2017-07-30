5321aaf739b41a850903b16c48a884be9e47576b
git status <spike.lu@emory.edu>
2017-07-29 23:03:08 -0700

    fixes

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -5,7 +5,7 @@ import StudentContainer from './studentContainer'
 import LoginContainer from './loginContainer'
 
 import Example from '../components/VoiceRecorder'
-import SketchFieldDemo from '../components/canvas'
+import SketchPad from '../components/canvas'
 
 
 export default class Routes extends React.Component {
@@ -17,7 +17,7 @@ export default class Routes extends React.Component {
     return(
       <div>
         <Switch>
-              <Route path="/" exact={true} component={SketchFieldDemo}/>
+              <Route path="/" exact={true} component={SketchPad}/>
               <Route path="/professor" component={ProfessorContainer}/>
               <Route path="/student" component={StudentContainer}/>
         </Switch>
