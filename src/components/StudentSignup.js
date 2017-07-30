1a663267f0a21d3313521514d82b393df9c46750
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-07-30 08:14:52 -0700

    fixed changes

--- src/components/StudentSignup.js
+++ src/components/StudentSignup.js
@@ -26,7 +26,7 @@ export default class StudentSignup extends React.Component {
   render() {
     return(
       <div className="row">
-        {this.state.shouldRedirect ? <Redirect to={"/studentCode"}/> : ""}
+        {this.state.shouldRedirect ? <Redirect to={"/student"}/> : ""}
         <br/><br/><br/>
         <div className="card-row-1 card-panel col s6 offset-s3">
           <h1>Create a new Student Profile</h1>
@@ -44,8 +44,8 @@ export default class StudentSignup extends React.Component {
             <form className="col s12">
               <div className="row">
                 <div className="input-field col s10 offset-s1">
-                  <input id="email" type="email" className="validate"/>
-                  <label htmlFor="email">Email</label>
+                  <input id="email" type="text" className="validate"/>
+                  <label htmlFor="email">Access Code</label>
                 </div>
               </div>
             </form>
