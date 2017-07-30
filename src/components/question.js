2b0aeba930fe4b5c7b57c5139fef7044f8dca222
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-07-29 18:44:12 -0700

    added cards

--- src/components/question.js
+++ src/components/question.js
@@ -8,7 +8,9 @@ export default class Question extends React.Component {
   render() {
     return(
       <li className="liBorder">
+        <div className="card-row-1 card-panel white card-panel-question-posted">
         <span className="nav-text">{this.props.username}: {this.props.question}</span>
+      </div>
       </li>
     )
   }
